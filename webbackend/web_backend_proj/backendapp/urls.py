2ce30f24593a14a629dfb79baf821af5ca3d3aa1
Martin Chorley <m.j.chorley@cs.cardiff.ac.uk>
2011-09-17 19:08:50 +0100

    crawl views done

--- webbackend/web_backend_proj/backendapp/urls.py
+++ webbackend/web_backend_proj/backendapp/urls.py
@@ -7,21 +7,21 @@ urlpatterns = patterns('',
     # params:
     # name              name for the crawl
     # description       description of the crawl
-    # startdatetime     start date of the time as seconds after epoch
+    # startdatetime     start date of the crawl as seconds after epoch
     # duration          duration of the crawl in minutes
     # leader            user_id of the crawl leader
     # venues            list of foursq_ids of venues on the crawl
     #
     # adds a crawl to the system. Returns the crawl_id
-    url( r'^crawl/(\d+)/', include( 'backendapp.urls' ) ),
+    url( r'^crawl/(\d+)/$', 'backendapp.views.crawl_info' ),
     # GET request
     #
     # returns info for the crawl
-    url( r'^crawl/list/$', include( 'backendapp.urls' ) ),
+    url( r'^crawl/list/$', 'backendapp.views.crawl_list' ),
     # GET request
     #
     # returns all crawls in the system
-    url( r'^crawl/(\d+)/add_venue/$', include( 'backendapp.urls' ) ),
+    url( r'^crawl/(\d+)/add_venue/$', 'backendapp.views.crawl_add_venue' ),
     # POST request.
     #
     # params:
@@ -29,26 +29,26 @@ urlpatterns = patterns('',
     # index         the index of the venue within the crawl
     #
     # inserts a venue to the crawl at the specified index
-    url( r'^crawl/(\d+)/remove_venue/$', include( 'backendapp.urls' ) ),
+    url( r'^crawl/(\d+)/remove_venue/$', 'backendapp.views.crawl_remove_venue' ),
     # POST request.
     #
     # params:
     # venue_id      the index of the venue to remove from the crawl
     #
     # removes the venue from the crawl
-    url( r'^crawl/(\d+)/delete/$', include( 'backendapp.urls' ) ),
+    url( r'^crawl/(\d+)/delete/$', 'backendapp.views.crawl_delete' ),
     # POST request.
     #
     # params:
-    # user_id       the ID of the requesting user
+    # user_id       the user id of the requesting user
     #
     # deletes the crawl
-    url( r'^crawl/(\d+)/add_user/$', include( 'backendapp.urls' ) ),
+    url( r'^crawl/(\d+)/add_user/$', 'backendapp.views.crawl_add_user' ),
     # POST request.
     #
     # params:
     # user_id       the user to add to the crawl
-    url( r'^crawl/(\d+)/remove_user/$', include( 'backendapp.urls' ) ),
+    url( r'^crawl/(\d+)/remove_user/$', 'backendapp.views.crawl_remove_user' ),
     # POST request
     #
     # params:
@@ -65,10 +65,13 @@ urlpatterns = patterns('',
     #
     # create a user
     url( r'^user/(\d+)/crawls/$', 'backendapp.views.user_id_crawls' ),
+    # GET request
+    #
+    # return the list of crawls the user has been on
     url( r'^user/list/$', 'backendapp.views.user_list' ),
     # GET request
     #
-    # returns the list of crawls the user has been on
+    # returns the list of users
     url( r'^checkin/$', include( 'backendapp.urls' ) )
     # POST request
     #
