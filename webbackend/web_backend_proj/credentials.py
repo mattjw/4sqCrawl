38027225230da3a1a71a36b7a75ec34afb4d02a5
Matt J Williams <spammable@nomovingparts.net>
2011-09-19 00:16:58 +0100

    test

--- webbackend/web_backend_proj/credentials.py
+++
@@ -1,9 +0,0 @@
-CLIENT_ID = "MTBEMOZIHCZQ3SIV41ZTIPPOXJBKS0XPNI35E2VB5FRKXHT4"
-CLIENT_SECRET = "BHKVMF5R43MDYBJMKJ2KEDANVNXJZXL04H0YIALI4Q2OIFQ4"
-CALLBACK_URL = "http://127.0.0.1:8000/callback"
-AUTHORIZE_URL = 'https://foursquare.com/oauth2/authenticate'
-ACCESS_TOKEN_URL = 'https://foursquare.com/oauth2/access_token'
-
-
-#https://fourcrawl.nomovingparts.net/callback
-#"https://fourcrawl.nomovingparts.net/callback"
